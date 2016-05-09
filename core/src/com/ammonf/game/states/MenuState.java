e8bdc5ae25b640fa194b704e519f82474ff1630f
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-05-08 17:30:54 -0700

    Texture objects created

--- core/src/com/ammonf/game/states/MenuState.java
+++ core/src/com/ammonf/game/states/MenuState.java
@@ -1,15 +1,21 @@
 package com.ammonf.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by Fritz on 5/6/2016.
  */
 public class MenuState extends State {
+    private Texture background;
+    private Texture playButton;
 
     // protected -> public
     public MenuState(GameStateManager gsm) {
         super(gsm); // Calls State constructor with gsm
+
+        background = new Texture("bg.png");
+        playButton = new Texture("play.png"); // Shortcut: Ctrl+D = dup line.
     }
 
     // protected -> public
