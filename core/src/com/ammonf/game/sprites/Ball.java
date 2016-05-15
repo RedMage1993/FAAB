f56f045ad113091277f121aaf672652759a8795b
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-05-14 18:41:44 -0700

    Added y position of Ball to be random

--- core/src/com/ammonf/game/sprites/Ball.java
+++ core/src/com/ammonf/game/sprites/Ball.java
@@ -1,14 +1,27 @@
 package com.ammonf.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
 
 /**
  * Created by Fritz on 5/14/2016.
  */
-public class Ball {
-    private Texture ball;
+public abstract class Ball {
+    // These are the possible locations for Ball
+    private static final int LEVELS[] = {300, 355, 245};
 
-    public Ball(float x, float y) {
-        //ball = new Texture();
+    protected Texture ball;
+    private Vector2 position;
+    private Random rand;
+
+    protected Ball(float x, String spritePath) {
+        ball = new Texture(spritePath);
+        rand = new Random(); // Used to set ball at random Y-axis
+
+        // Choose a random location
+        // RNG between 0 (inclusive) and 3 (exclusive)
+        position = new Vector2(x, LEVELS[rand.nextInt(3)]);
     }
 }
