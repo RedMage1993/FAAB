51c1170f804f752cf17b8c0897aad02fd15b49b1
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-05-14 18:11:24 -0700

    Created Ball class and added sprites for good and bad ball

---
+++ core/src/com/ammonf/game/sprites/Ball.java
@@ -0,0 +1,14 @@
+package com.ammonf.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Fritz on 5/14/2016.
+ */
+public class Ball {
+    private Texture ball;
+
+    public Ball(float x, float y) {
+        //ball = new Texture();
+    }
+}
