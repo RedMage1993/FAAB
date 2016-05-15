fa595f18aee6b86f95285c5bbfe42259bab4e35b
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-05-14 19:06:41 -0700

    Defined GoodBall and BadBall subclasses of Ball

---
+++ core/src/com/ammonf/game/sprites/BadBall.java
@@ -0,0 +1,13 @@
+package com.ammonf.game.sprites;
+
+/**
+ * Created by Fritz on 5/14/2016.
+ */
+public class BadBall extends Ball {
+    public BadBall(float x) {
+        super(x, "ball-bad1.png");
+    }
+
+    // We may add some GoodBall specific methods or data below
+    // For example, decrease points? Or lose game.
+}
---
+++ core/src/com/ammonf/game/sprites/GoodBall.java
@@ -0,0 +1,13 @@
+package com.ammonf.game.sprites;
+
+/**
+ * Created by Fritz on 5/14/2016.
+ */
+public class GoodBall extends Ball {
+    public GoodBall(float x) {
+        super(x, "ball-good1.png");
+    }
+
+    // We may add some GoodBall specific methods or data below
+    // For example, increase points?
+}
